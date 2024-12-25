55c86ffb61ff5d07e185582cf88389952324f4f2
Khritish Kumar Behera <khritish.official@gmail.com>
2024-12-25 12:09:50 +0530

    Server code changed

--- Server.java
+++ Server.java
@@ -1,6 +1,10 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 import com.sun.net.httpserver.*;
 
@@ -10,6 +14,7 @@ public class Server
     {
         HttpServer server = HttpServer.create(new InetSocketAddress(ip_address,port),0);
         server.createContext("/", new RootHandler());
+        server.createContext("/upload", new UploadHandler());
         server.start();
         System.out.println("Server started");
     } 
@@ -18,11 +23,43 @@ public class Server
         @Override
         public void handle(HttpExchange exchange) throws IOException
         {
-            String response = "Hello message from the hello handler";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            Path htmlPath = Path.of("home.html");
+            String response = Files.readString(htmlPath);
+
+            // Set headers and send response
+            exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
+            try (OutputStream os = exchange.getResponseBody()) 
+            {
+                os.write(response.getBytes());
+            }
+        }
+    }
+    static class UploadHandler implements HttpHandler
+    {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException 
+        {
+            if ("POST".equalsIgnoreCase(exchange.getRequestMethod())) 
+            {
+                // parse headers to extract original file name
+                String contentDisposition = exchange.getRequestHeaders().getFirst("Content-Disposition");
+                String originalFileName = "uploaded_fly_file";
+                System.out.println(exchange.getRequestHeaders().isEmpty());
+                System.out.println(contentDisposition);
+                if(contentDisposition != null && contentDisposition.contains("filename="))
+                {
+                    originalFileName = contentDisposition.split("filename=")[1].replace("\"","").trim();
+                }
+                System.out.println(originalFileName);
+                String unique_file_name = "uploads/" + originalFileName + "_" + System.currentTimeMillis();
+            } 
+            else 
+            {
+                exchange.sendResponseHeaders(405, -1); // Method not allowed
+            }
         }
     }
 }
+
